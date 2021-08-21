8064314d5c87b56a5c42a9bf03b229a771461318
Tom Drapes <tjdrapes@gmail.com>
2021-08-21 14:57:38 +1000

    initial commit

---
+++ fcc/src/App.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import {Grid} from '@material-ui/core'
+import { GameInfoCard } from './GameInfoCard';
+import './App.css';
+
+type CardInfo = {
+  heading: string;
+  subHeading: string;
+  imgUrl: string;
+  description: string;
+}
+
+function App() {
+
+  const appCards: Array<CardInfo> = [
+    {
+      heading: 'Tetris',
+      subHeading: "This is tetris",
+      imgUrl: "/image1.svg",
+      description: "Tetris is a game"
+     },
+     {
+      heading: 'Sudoku',
+      subHeading: "This is sudoku",
+      imgUrl: "/image2.svg",
+      description: "Sudoku is a game"
+     },
+     {
+      heading: 'Hangman',
+      subHeading: "This is hangman",
+      imgUrl: "/image3.svg",
+      description: "Hangman is a game"
+     },
+     {
+      heading: 'Trivia',
+      subHeading: "This is trivia",
+      imgUrl: "/image4.svg",
+      description: "Trivia is a game"
+     },
+     {
+      heading: 'Asteroids',
+      subHeading: "This is asteroids",
+      imgUrl: "/image5.svg",
+      description: "Asteroids is a game"
+     }
+    ];
+
+  return (
+    <div style={{width: "100vw", height: "100vh", padding: "16px", overflowY: "scroll", backgroundColor: "lightgray"}}>
+      <Grid container direction="row" spacing={3} justifyContent="space-evenly">
+        {appCards.map((item: CardInfo) => (
+          <Grid item>
+            <GameInfoCard heading={item.heading} subHeading={item.subHeading} imgUrl={item.imgUrl} description={item.description} />
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
+}
+
+export default App;
---
+++ fcc/src/GameInfoCard.tsx
@@ -0,0 +1,111 @@
+import React, {useState} from 'react';
+import {
+   Card,
+   CardHeader,
+   Avatar,
+   IconButton,
+   CardMedia,
+   Typography,
+   CardActions,
+   CardContent,
+   Collapse,
+   makeStyles,
+} from '@material-ui/core'
+import MoreVertIcon from "@material-ui/icons/MoreVert"
+import FavoriteIcon from "@material-ui/icons/Favorite"
+import ShareIcon from "@material-ui/icons/Share"
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
+import './App.css';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: "red",
+  },
+}));
+
+interface Props {
+  heading: string;
+  subHeading: string;
+  description: string;
+  imgUrl: string;
+}
+
+export const GameInfoCard = (props: Props) => {
+  const {heading, subHeading, description, imgUrl} = props;
+
+  const classes = useStyles();
+
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={heading}
+        subheader={subHeading}
+      />
+      <CardMedia
+        className={classes.media}
+        image={imgUrl}
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {description}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <IconButton
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Method:</Typography>
+          <Typography paragraph>
+            Add more info here
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  )
+}
