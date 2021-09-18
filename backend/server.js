96200fecf21fd8058276a54541f3cfc12534d3db
recelis <jackymlui@live.com>
2021-09-18 11:37:32 +1000

    changed port 9000

--- backend/server.js
+++ backend/server.js
@@ -19,27 +19,26 @@ app.use(cors());
 app.use(express.json()); // this is needed for fetch calls from app
 app.use(express.urlencoded({ extended: true })); // don't know if extended needs to be true
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 9000;
 
 //Socket.io
-const http = require('http').Server(app);
-const io = require('socket.io')(http, {
-  cors:
-  {
+const http = require("http").Server(app);
+const io = require("socket.io")(http, {
+  cors: {
     origin: "http://localhost:3000",
-    methods: ["GET", "POST"]
-  }
+    methods: ["GET", "POST"],
+  },
 });
 
 let clients = [];
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('test', function(msg){
-    console.log('working: ', msg);
+io.on("connection", function (socket) {
+  console.log("a user connected");
+  socket.on("test", function (msg) {
+    console.log("working: ", msg);
   });
 });
 //io.listen(9000)
-http.listen(9000);
+// http.listen(9000);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -54,6 +53,6 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "../fcc/build", "index.html"));
   });
 }
-app.listen(process.env.PORT || port, () =>
+http.listen(process.env.PORT || port, () =>
   console.log(`server is running on ${port}`)
 );
